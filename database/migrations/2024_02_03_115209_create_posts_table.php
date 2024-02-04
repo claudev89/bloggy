e621e7f9a89207345aef0df9117ef743382eac8a
claudioalarcon89 <menaresclaudio08@gmail.com>
2024-02-03 23:34:54 -0300

    Creados todos los modelos, sus migraciones correspondientes e incluso las de los roles y permisos

---
+++ database/migrations/2024_02_03_115209_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 500);
+            $table->string('image', 255);
+            $table->text('body');
+            $table->unsignedBigInteger('autor')->nullable();
+            $table->boolean('borrador');
+            $table->timestamps();
+
+            $table->foreign('autor')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
